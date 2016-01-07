cd08ba6e3dcc48532f3e9c3312efde0494828dd5
FlorianLangeder <flo.langeder@gmail.com>
2016-01-07 16:26:47 +0100

    Added: Polygon Drawing

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,21 +9,47 @@ import java.util.Map;
  */
 public class GameBoard {
 
+    private GameBoardFrame boardFrame;
     private HashMap<String, Territory> territories;
     private HashMap<String, Continent> continents;
 
     public GameBoard() {
-        GameBoardFrame boardFrame = new GameBoardFrame();
         MapLoader loader = new MapLoader("world.map");
+
         territories = loader.getTerritories();
         continents = loader.getContinents();
-        for(Map.Entry<String, Territory> entry : territories.entrySet()) {
+
+        boardFrame = new GameBoardFrame();
+        addPatchesToFrame();
+        boardFrame.showFrame();
+
+        addFrameListener();
+
+        /*for(Map.Entry<String, Territory> entry : territories.entrySet()) {
             System.out.println(entry.getKey());
             System.out.println(entry.getValue().toString());
         }
         for(Map.Entry<String, Continent> entry : continents.entrySet()) {
             System.out.println(entry.getKey());
             System.out.println(entry.getValue().toString());
+        }*/
+    }
+
+    private void addPatchesToFrame() {
+        for(Territory terr: territories.values()) {
+            boardFrame.addPolygons(terr.getPatches());
         }
+    }
+
+    private void addFrameListener() {
+        boardFrame.mainPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e.getX() + " "+e.getY());
+                //TODO: returns always null
+                Component panel = (Component)e.getSource();
+                System.out.println(panel.getName());
+            }
+        });
     }
 }
--- src/GameBoardFrame.java
+++ src/GameBoardFrame.java
@@ -1,15 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Florian Langeder on 23.12.15.
  */
 public class GameBoardFrame extends JFrame{
 
+    private ArrayList<Polygon> polygonList = new ArrayList<>();
+    public JPanel mainPanel;
+
     public GameBoardFrame() {
         super("All Those Territories");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showFrame() {
         setSize(1250, 650);
         setResizable(false);
+        mainPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.BLUE);
+                for(Polygon pol : polygonList) {
+                    g.drawPolygon(pol);
+                }
+            }
+        };
+        mainPanel.setSize(1250, 650);
+        mainPanel.setBackground(Color.CYAN);
+        getContentPane().add(mainPanel);
         setVisible(true);
+    }
+
+    public void addPolygons(ArrayList<ArrayList<Point>> points) {
+        for(ArrayList<Point> pointList : points) {
+            addPolygon(pointList);
+        }
+    }
+
+    private void addPolygon(ArrayList<Point> points) {
+        Polygon poly = new Polygon();
+        for(Point p : points) {
+            poly.addPoint((int)(p.getX()), (int) p.getY());
+        }
+        polygonList.add(poly);
     }
 }
